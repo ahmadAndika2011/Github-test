f5ed37f400e1541deb3e93de546f0da296417181
ahmadAndika2011 <andika.work.1111@gmail.com>
2026-01-11 14:52:53 +0700

    update

--- app.mjs
+++ app.mjs
@@ -1,7 +1,14 @@
 const btn = document.createElement("button");
-btn.textContent = "click me";
+btn.innerText = "click me";
 
 btn.onclick = () => {
+  let name = prompt("what is your name: ");
+  alert(`hello ${name}`);
+};
+
+
+const btn_2 = document.getElementById("btn_click")
+btn_2.onclick = () => {
   let name = prompt("what is your name: ");
   alert(`hello ${name}`);
 };
